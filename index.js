0baacdc7cd135021271f5ec2dc2b8a91f53d2789
Evan Mulrooney <evanmulrooney@gmail.com>
2021-03-29 18:51:52 -0400

    2.1 Set up all stub endpoints for backend

--- index.js
+++ index.js
@@ -14,26 +14,93 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
 
+/**
+ * While multiple walls aren't really a thing, this is fine.
+ */
 app.get('/debug', async (req, res) => {
-    let content = await db.getDebugContent();
+    let content = await db.getWall();
     res.send(content);
-})
+});
 
-app.get('/debug/:id', (req, res) => {
-    const id = Number(req.params.id);
-    //const dbc = debugContent.find(dbc => dbc.id === id);
+/**
+ * TODO Get a list of walls: title, color/icon, ID. 
+ */
+app.get("/wall", async (req, res) => {
+    let content = console.log("Not yet implemented.");
+    res.send(content);
+});
 
-    if (!dbc) {
-        res.send({ "status": "Couldn't find item!" });
-    }
+/**
+ * Get a particular wall. Wall data (like title, bg color, icon, ID) as well as the collection of embedded panels.
+ */
+app.get("/wall/:wallId", async (req, res) => {
+    const wallId = Number(req.params.wallId);
+    let content = await db.getWall(wallId);
+    res.send(content);
+});
 
-    res.send(dbc);
-})
-
-app.post('/wall/:wallId/:panelId', jsonParser, (req, res) => {
+/**
+ * TODO Get a particular panel from a wall. Used to reset a modified (but not yet saved) panel. 
+ */
+app.get("/wall/:wallId/:panelId", async (req, res) => {
     const wallId = Number(req.params.wallId);
     const panelId = Number(req.params.panelId);
+    let content = console.log("Not yet implemented.");
+    res.send(content);
+});
 
+/**
+ * TODO Create a new wall. Body should include wall data (title, icon, iconColor, bgColor).
+ */
+app.post("/wall", async (req, res) => {
+    let content = console.log("Not yet implemented.");
+    res.send(content);
+});
 
+/**
+ * TODO Create a new panel on wall. Body should include panel data (sizing, type, body, title/subtitle)
+ */
+app.post("/wall/:wallId", async (req, res) => {
+    const wallId = Number(req.params.wallId);
+    let content = console.log("Not yet implemented.");
+    res.send(content);
+});
 
+/**
+ * TODO Update the wall data. May be called in two spots (wall editor and on the general wall view page) to handle wall data as well 
+ * as panel layouts. 
+ */
+app.put("/wall/:wallId", async (req, res) => {
+    const wallId = Number(req.params.wallId);
+    let content = console.log("Not yet implemented.");
+    res.send(content);
+});
+
+/**
+ * TODO Update a particular panel on a particular wall. Might have (sub)title, body text, sizing, type changes. 
+ */
+app.put("/wall/:wallId/:panelId", async (req, res) => {
+    const wallId = Number(req.params.wallId);
+    const panelId = Number(req.params.panelId);
+    let content = console.log("Not yet implemented.");
+    res.send(content);
+});
+
+/**
+ * TODO Delete an entire wall.
+ */
+app.delete("/wall/:wallId/", async (req, res) => {
+    const wallId = Number(req.params.wallId);
+    let content = console.log("Not yet implemented.");
+    res.send(content);
+});
+
+/**
+ * TODO Delete a particular panel on a wall.
+ */
+app.put("/wall/:wallId/:panelId", async (req, res) => {
+    const wallId = Number(req.params.wallId);
+    const panelId = Number(req.params.panelId);
+    let content = console.log("Not yet implemented.");
+    res.send(content);
 });
